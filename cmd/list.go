f77383c59262266f4ea20cc8008e3de58647dfd4
Alberto Cavalcante <54247214+albertocavalcante@users.noreply.github.com>
2026-01-22 23:27:39 -0800

    feat: integrate go-bzlmod library
    - Use go-bzlmod for MODULE.bazel parsing and BCR access
    - Implement list command with tabular output
    - Implement info command to fetch module details from BCR
    - Implement add command to append dependencies
    - Remove internal parser (replaced by go-bzlmod)

--- cmd/list.go
+++ cmd/list.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 
+	gobzlmod "github.com/albertocavalcante/go-bzlmod"
 	"github.com/spf13/cobra"
 )
 
@@ -10,11 +13,7 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List dependencies in MODULE.bazel",
 	Long:  `List all bazel_dep entries in your MODULE.bazel file.`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		// TODO: implement
-		fmt.Println("Listing dependencies...")
-		return nil
-	},
+	RunE:  runList,
 }
 
 var (
@@ -24,4 +23,53 @@ var (
 func init() {
 	listCmd.Flags().BoolVar(&listOutdated, "outdated", false, "Show only outdated dependencies")
 	rootCmd.AddCommand(listCmd)
+}
+
+func runList(cmd *cobra.Command, args []string) error {
+	modulePath, err := findModuleFile()
+	if err != nil {
+		return err
+	}
+
+	info, err := gobzlmod.ParseModuleFile(modulePath)
+	if err != nil {
+		return fmt.Errorf("failed to parse MODULE.bazel: %w", err)
+	}
+
+	if info.Name != "" {
+		fmt.Printf("Module: %s", info.Name)
+		if info.Version != "" {
+			fmt.Printf(" (%s)", info.Version)
+		}
+		fmt.Println()
+		fmt.Println()
+	}
+
+	if len(info.Dependencies) == 0 {
+		fmt.Println("No dependencies found.")
+		return nil
+	}
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+	fmt.Fprintln(w, "NAME\tVERSION\tDEV")
+
+	for _, dep := range info.Dependencies {
+		dev := ""
+		if dep.DevDependency {
+			dev = "yes"
+		}
+		fmt.Fprintf(w, "%s\t%s\t%s\n", dep.Name, dep.Version, dev)
+	}
+
+	return w.Flush()
+}
+
+func findModuleFile() (string, error) {
+	// Check current directory first
+	if _, err := os.Stat("MODULE.bazel"); err == nil {
+		return "MODULE.bazel", nil
+	}
+
+	// TODO: walk up to find workspace root
+	return "", fmt.Errorf("MODULE.bazel not found in current directory")
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module github.com/albertocavalcante/bz
 
 go 1.25.5
 
-require github.com/spf13/cobra v1.10.2
+require (
+	github.com/albertocavalcante/go-bzlmod v0.0.0-20250609045524-18d63095a7cd
+	github.com/spf13/cobra v1.10.2
+)
 
 require (
+	github.com/bazelbuild/buildtools v0.0.0-20250602201422-b1e23f1025b8 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
+	golang.org/x/mod v0.25.0 // indirect
 )
